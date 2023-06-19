bf522d65bc8fd0386eeb624c0f260588353843ef
alexislan <alexislanchiotti13@gmail.com>
2023-06-19 14:06:22 -0300

    añadi que cuando se ingrese a la pagina se carguen las pelis mas populares

--- popular.js
+++ popular.js
@@ -5,31 +5,10 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYjRmYWNmMTBjNDY1N2U2YWI0YjJhYWQ2YTBjZTA3NyIsInN1YiI6IjY0OGU4Mjc3MmY4ZDA5MDBlMzg1YTg3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PQtDV8AM3HocDZ_VMGdQ3WxlvFQNzhPmxCAV191I50M'
     }
   };
-
-
-let input = document.getElementById("inpu");
-let html = document.querySelector("html"); 
-let prueba = document.querySelector(".prueba");
-let URL_search = `https://api.themoviedb.org/3/search/movie?query=fast%20x&include_adult=false&language=en-US&page=1`;
-
-function buscar(){
-
-
-    let palabra = input.value;
-    let arrayP = palabra.split(" ");
-    if(palabra != ""){
-        if(arrayP.length == 1){
-            URL_search = `https://api.themoviedb.org/3/search/movie?query=${palabra}&include_adult=false&language=en-US&page=1`
-            
-        }
-        else{
-            let newstring = arrayP.join("%20");
-            console.log(newstring)
-            URL_search = `https://api.themoviedb.org/3/search/movie?query=${newstring}&include_adult=false&language=en-US&page=1`
-        }
-    }
+let url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+function pelisPop(){
     function getCharacters(done){
-        const results = fetch(URL_search,options);
+        const results = fetch(url,options);
         results
         .then(response => response.json())
         .then(data => {done(data)});
@@ -52,7 +31,7 @@ function buscar(){
         )
         
         const main = document.querySelector(".containerPelis");
-        main.append(article)
+        main.append(article);
     })
     });
 }
