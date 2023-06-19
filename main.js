a26d9e9d1b2ae7be11008f147cbdb3a026e79e16
alexislan <alexislanchiotti13@gmail.com>
2023-06-19 02:53:49 -0300

    logre que el search funcione

--- main.js
+++ main.js
@@ -7,32 +7,50 @@ const options = {
 };
 
 
-// let input = document.getElementById("inpu");
-
+let input = document.getElementById("inpu");
+let html = document.querySelector("html"); 
+let prueba = document.querySelector(".prueba");
 let URL_search = `https://online-movie-database.p.rapidapi.com/title/v2/find?title=spider&titleType=movie&limit=20&sortArg=boxoffice_gross_us%2Casc`;
-function getCharacters(done){
-    const results = fetch(URL_search,options);
-    results
-    .then(response => response.json())
-    .then(data => {done(data)});
+
+function buscar(){
+    let palabra = input.value;
+    let arrayP = palabra.split(" ");
+    if(palabra != ""){
+        if(arrayP.length == 1){
+            URL_search = `https://online-movie-database.p.rapidapi.com/title/v2/find?title=${input.value}&titleType=movie&limit=20&sortArg=boxoffice_gross_us%2Casc`
+            
+        }
+        else{
+            let newstring = arrayP.join("%20");
+            console.log(newstring)
+            URL_search = `https://online-movie-database.p.rapidapi.com/title/v2/find?title=${newstring}&titleType=movie&limit=20&sortArg=boxoffice_gross_us%2Casc`
+        }
 }
+    function getCharacters(done){
+        const results = fetch(URL_search,options);
+        results
+        .then(response => response.json())
+        .then(data => {done(data)});
+    }
 
 
-getCharacters(data => {
-console.log(data)
-data.results.forEach(pelicula => {
-    const article = document.createRange().createContextualFragment(
-        `<div class="peli"> 
-        <img src="${pelicula.image.url}" alt="">
-        <div>
-          <p class="tituloPeli">${pelicula.title}</p>
-          <p class="añoPeli">${pelicula.year}</p>
+    getCharacters(data => {
+    console.log(data);
+    data.results.forEach(pelicula => {
+        const article = document.createRange().createContextualFragment(
+            `
+        <div class="peli"> 
+            <img src="${pelicula.image.url}" alt="">
+            <div>
+            <p class="tituloPeli">${pelicula.title}</p>
+            <p class="añoPeli">${pelicula.year}</p>
+            </div>
         </div>
-      </div>
-        `
-    )
-
-    const main = document.querySelector(".containerPelis");
-    main.append(article)
-})
-});
+            `
+        )
+        
+        const main = document.querySelector(".containerPelis");
+        main.append(article)
+    })
+    });
+}
