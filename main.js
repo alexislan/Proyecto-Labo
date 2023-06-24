57e2515ab8b10e1b0384f357a15f62ca988dc570
agus-diaz <agustindiaz200366@gmail.com>
2023-06-24 20:41:13 -0300

    trailer

--- main.js
+++ main.js
@@ -102,7 +102,10 @@ function buscar(){
                         <img src="https://image.tmdb.org/t/p/original/${pelicula.poster_path}" alt="">
                         <div>
                         <p class="tituloPeli">${pelicula.title}</p>
-                        <p class="añoPeli">${pelicula.release_date}</p>
+                        <div class = "puntajeP">
+                            <span class="${getColor(pelicula.vote_average)}">${pelicula.vote_average}</span>
+                            <img src="imagenes/star-solid-24.png">
+                            </div>
                         </div>
                     </div>
                 </a>
@@ -135,26 +138,107 @@ getCharacters(data => {
     console.log(data)
     const article = document.createRange().createContextualFragment(
         `
-        
             <div class="peli"> 
                 <img src="https://image.tmdb.org/t/p/original/${data.poster_path}" alt="">
                 <div>
                     <p class="tituloPeli">${data.title}</p>
                     <p class="añoPeli">${data.release_date}</p>
                 </div>
+                <button class="trailer" id="${data.id}"> Trailer </button>
             </div>
-
             <p>${data.overview}</p>
         `
     )
-    main.append(article)
+    
+        main.append(article)
         //hace que vuelva atras en la pagina
     window.addEventListener('popstate', function (e) {
-        window.location.assign("index.html");
+        window.location.assign("index.html");        
     });
+    document.getElementById(id).addEventListener('click',()=>{
+         openNav(data)
+    })
 })
 
 }
+
+const overlayContent = document.getElementById('overlay-content')
+function openNav(data) {
+    let id=data.id
+    fetch('https://api.themoviedb.org/3/movie/'+id+'/videos?'+'api_key=bb4facf10c4657e6ab4b2aad6a0ce077').then(res=>res.json())
+    .then(videoData=>{
+        console.log(videoData)
+        if(videoData){
+            document.getElementById("myNav").style.width = "100%";
+            if(videoData.results.length>0){
+              var embed=[];
+              videoData.results.forEach(video=>{
+                let {name, key, site}=video;
+                if(site == 'YouTube'){
+                    embed.push(`
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" 
+                title="${name}" class="embed hide" frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; 
+                encrypted-media; gyroscope; picture-in-picture; 
+                web-share" allowfullscreen></iframe>
+                    `)
+
+                }            
+              })             
+              overlayContent.innerHTML=embed.join('')
+              activeSlide=0;
+              showVideos();
+            }else{
+               overlayContent.innerHTML=`<h1>No Results Found</h1>`
+            }
+        }
+    })
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+    const iframes = document.getElementsByTagName('iframe');
+    if (iframes !== null) {
+      for (let i = 0; i < iframes.length; i++) {
+        iframes[i].src = iframes[i].src;
+      }
+    }
+  }
+
+  var activeSlide=0;
+  var totalVideos=0;
+
+function showVideos(){
+    let embedClasses=document.querySelectorAll('.embed')
+    totalVideos= embedClasses.length;
+    embedClasses.forEach((embedTag, idx)=>{
+      if(activeSlide==idx){
+        embedTag.classList.add('show')
+        embedTag.classList.remove('hide')
+      }else{
+        embedTag.classList.add('hide')
+        embedTag.classList.remove('show')
+      }
+    })
+}
+const leftArrow=document.getElementById('left-arrow')
+const rightArrow=document.getElementById('right-arrow')
+leftArrow.addEventListener('click',()=>{
+    if(activeSlide>0){
+        activeSlide--;
+    }else{ 
+           activeSlide=totalVideos-1;
+    }
+    showVideos();
+})
+rightArrow.addEventListener('click',()=>{
+    if(activeSlide<(totalVideos-1)){
+        activeSlide++;
+    }else{ 
+           activeSlide=0;
+    }
+    showVideos();
+})
 linkcat = '';
 function pelisCat(query){
 
@@ -182,7 +266,10 @@ function pelisCat(query){
                                 <img src="https://image.tmdb.org/t/p/original/${pelicula.poster_path}" alt="">
                                 <div>
                                     <p class="tituloPeli">${pelicula.title}</p>
-                                    <p class="añoPeli">${pelicula.release_date}</p>
+                                    <div class = "puntajeP">
+                            <span class="${getColor(pelicula.vote_average)}">${pelicula.vote_average}</span>
+                            <img src="imagenes/star-solid-24.png">
+                            </div>
                                 </div>
                             </div>
                         </a>
